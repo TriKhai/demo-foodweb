38a25edf894a24c3f47f46238711045a6b05dd24
TriKhai <lytrikhaikg2021@gmail.com>
2023-09-15 14:19:26 +0700

    upload src

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Routes, Route } from 'react-router-dom'
+import Navbar from "./page/Navbar";
+import Home from './page/Home';
+import Items from './page/Foodpage/Items';
+import Test from './page/Beverage/test';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+
+        <Route path="/cream" element={<Items/>}/>
+        <Route path="/bread" element={<Items/>}/>
+        <Route path="/gato" element={<Items/>}/>
+        <Route path="/beverage" element={<Items/>}/>
+
+        {/* <Route path="/beverage" element={<Test />}/> */}
+      </Routes>
+    </>
+  );
+}
+
+export default App;
